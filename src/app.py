1c3cf27c3c93ee7c1df6f23f584ecd4ab973ca3e
dylan <dylanhogg@gmail.com>
2020-12-10 17:57:24 +1100

    Black formatting.

--- src/app.py
+++ src/app.py
@@ -8,12 +8,8 @@ from library import env, log
 def main(required_arg: str, optional_arg: str = None) -> None:
     logger.info(f"Hello! required_arg = {required_arg}, optional_arg = {optional_arg}")
     logger.info(f"PYTHONPATH = {env.get('PYTHONPATH', 'Not set')}")
-    logger.info(
-        f"LOG_STDERR_LEVEL = {env.get('LOG_STDERR_LEVEL', 'Not set. Copy `.env_template` to `.env`')}"
-    )
-    logger.info(
-        f"LOG_FILE_LEVEL = {env.get('LOG_FILE_LEVEL', 'Not set. Copy `.env_template` to `.env`')}"
-    )
+    logger.info(f"LOG_STDERR_LEVEL = {env.get('LOG_STDERR_LEVEL', 'Not set. Copy `.env_template` to `.env`')}")
+    logger.info(f"LOG_FILE_LEVEL = {env.get('LOG_FILE_LEVEL', 'Not set. Copy `.env_template` to `.env`')}")
 
     for i in tqdm(range(5)):
         sleep(0.1)
